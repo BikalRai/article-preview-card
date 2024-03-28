1781b7825dc112e85f761510ad8eb5b0d34f4213
Bikal Rai <bikalrai2785@gmailcom>
2024-03-28 18:14:15 +0545

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+import CardImage from './CardImage';
+
+const Card = () => {
+  return (
+    <div className="card">
+      <CardImage />
+      <div className="card__details">
+        <h1 className="card__details--header">
+          Shift the overall look and feel by adding these wonderful touches to
+          furniture in your home
+        </h1>
+        <p className="card__details--body">
+          Ever been in a room and felt like something was missing? Perhaps it
+          felt slightly bare and uninviting. I’ve got some simple tips to help
+          you make any room feel complete.
+        </p>
+        <div className="card__details--info">
+          <div className="user">
+            <img src="" alt="" className="avatar" />
+            <div className="user__details">
+              <p>Michelle Appleton</p>
+              <p>28 Jun 2020</p>
+            </div>
+          </div>
+          <button className="btn">share</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/CardImage.jsx
@@ -0,0 +1,11 @@
+import drawers from '../assets/images/drawers.jpg';
+
+const CardImage = () => {
+  return (
+    <div className="card__img">
+      <img src={drawers} alt="Drawers image" />
+    </div>
+  );
+};
+
+export default CardImage;
